803ad0e8856025ed0ca19053370ef6fb2ef592e4
jensjvh <43621451+jensjvh@users.noreply.github.com>
2026-03-19 07:32:46 +0200

    Initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+require("config.lazy")
+
+vim.cmd.colorscheme("onedark")
+
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.number = true
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
