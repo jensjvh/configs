4bdb111d70bbc1d2cf2fd98ddd7e0ccba2a18f25
jensjvh <43621451+jensjvh@users.noreply.github.com>
2026-09-08 14:52:50 +0300

    Add zsh-autosuggestions

--- setup_on_fedora.sh
+++ setup_on_fedora.sh
@@ -31,6 +31,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ###########
 
 # foot
